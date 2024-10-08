4cf4ec351b522bee593bfdd775214494ba8ffdc6
Sharmavip <vipinhmr@yahoo.com>
2024-10-07 23:37:49 -0400

    Create terraform.tf
    vcs driven

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+
+  cloud {
+   organization = "terraform_coll"
+
+   workspaces {
+     name = "terraformproject"
+   }
+  }
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
